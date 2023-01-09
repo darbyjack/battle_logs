2e346271e35b29d58a8cef338386453967eddf98
darbyjack <admin@glaremasters.me>
2023-01-09 16:36:32 -0600

    Updated Nitro Preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     nitro: {
-      preset: "cloudflare-pages",
+      preset: "node-server",
     },
     css: [
       "vuetify/lib/styles/main.sass",
